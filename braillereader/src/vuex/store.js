0e839537b5a225e4280bdeedf687e96c03a3d4ce
JuppKuzal <pepa.kousal@gmail.com>
2021-01-24 15:24:54 +0100

    vuex colors removed

--- braillereader/src/vuex/store.js
+++ braillereader/src/vuex/store.js
@@ -10,20 +10,6 @@ const store = new Vuex.Store({
         windowWidth: window.innerWidth,
         phoneWidthBorder: 600,
         smallPhoneWidthBorder: 400,
-
-        colors: {
-            lightModeLighterColor: '#f0e5e5',
-            lightModeDarkerColor: '#B3B3B3',
-
-            darkModeLighterColor: '#404040',
-            darkModeDarkerColor: '#181818',
-
-            disabledTextColor: '#525252',
-            
-            cardGreen: 'rgb(64,64,64)',
-
-
-        }
     },
     mutations: {
         switchMode (state) {
